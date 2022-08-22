6e1ab2429e163f41e327d3ebfae5bb30244a3ba9
Jerrita <jerrita@Jerrita-Mac.lan>
2022-08-22 20:36:21 +0800

    Add: FreeDisk

--- scripts/01-GetRepo.sh
+++ scripts/01-GetRepo.sh
@@ -2,7 +2,7 @@ rls_tag="20220716"
 rls_file="https://github.com/coolsnowwolf/lede/archive/refs/tags/${rls_tag}.zip"
 
 echo "Downloading file..."
-wget ${rls_file} -O lede.zip > /dev/null
+wget ${rls_file} -O lede.zip > /dev/null 2>&1
 echo "Decompressing..."
 unzip lede.zip > /dev/null
 mv lede-${rls_tag} lede
--- scripts/02-Package.sh
+++ scripts/02-Package.sh
@@ -9,3 +9,7 @@ sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqba
 
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/TiSATO/g' package/base-files/files/bin/config_generate
+
+# Swap wan & lan
+sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
