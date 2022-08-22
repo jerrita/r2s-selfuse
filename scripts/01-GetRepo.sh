ec385804b82f38a821280570e0b80254420a0dcd
jerrita <je5r1ta@icloud.com>
2022-08-22 12:24:43 +0800

    fix

--- scripts/01-GetRepo.sh
+++ scripts/01-GetRepo.sh
@@ -1,8 +1,10 @@
 rls_tag="20220716"
 rls_file="https://github.com/coolsnowwolf/lede/archive/refs/tags/${rls_tag}.zip"
 
-wget ${rls_file} -O lede.zip
-unzip lede.zip
+echo "Downloading file..."
+wget ${rls_file} -O lede.zip > /dev/null
+echo "Decompressing..."
+unzip lede.zip > /dev/null
 mv lede-${rls_tag} lede
 
 echo "rls_tag=${rls_tag}" >> $GITHUB_ENV
