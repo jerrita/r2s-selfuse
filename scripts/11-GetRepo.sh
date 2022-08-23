e1a60563d927b45c0bdf9fb1a1f3ba0f07def00c
jerrita <je5r1ta@icloud.com>
2022-08-23 12:24:29 +0800

    IPv6 Support for LEDE

--- scripts/11-GetRepo.sh
+++ scripts/11-GetRepo.sh
@@ -1,7 +1,7 @@
 # Using SDK
 
-rls_code="21.02.3"
-sdk_name="openwrt-sdk-21.02.3-rockchip-armv8_gcc-8.4.0_musl.Linux-x86_64"
+rls_code="22.03.0-rc6"
+sdk_name="openwrt-sdk-22.03.0-rc6-rockchip-armv8_gcc-11.2.0_musl.Linux-x86_64.tar.xz"
 sdk_file="https://downloads.openwrt.org/releases/${rls_code}/targets/rockchip/armv8/${sdk_name}.tar.xz"
 
 echo "Downloading SDK..."
