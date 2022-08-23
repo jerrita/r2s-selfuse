4e14ab519953a9cc7f100f3768f3cc803bdcd7f3
jerrita <je5r1ta@icloud.com>
2022-08-23 11:12:44 +0800

    Remove UPX

--- scripts/11-GetRepo.sh
+++ scripts/11-GetRepo.sh
@@ -5,7 +5,7 @@ sdk_name="openwrt-sdk-21.02.3-rockchip-armv8_gcc-8.4.0_musl.Linux-x86_64"
 sdk_file="https://downloads.openwrt.org/releases/${rls_code}/targets/rockchip/armv8/${sdk_name}.tar.xz"
 
 echo "Downloading SDK..."
-wget ${sdk_file} -O sdk.tar.xz
+wget ${sdk_file} -O sdk.tar.xz > /dev/null 2>&1
 echo "Decompressing..."
 tar xf sdk.tar.xz
 mv ${sdk_name} openwrt
